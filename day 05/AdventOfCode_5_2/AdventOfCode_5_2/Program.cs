2cc8d035d68222836b2c5ee6a42b86f1215a8eab
NameAcht <appeljohannes21@gmail.com>
2023-02-28 22:24:32 +0100

    5

--- day 05/AdventOfCode_5_2/AdventOfCode_5_2/Program.cs
+++ day 05/AdventOfCode_5_2/AdventOfCode_5_2/Program.cs
@@ -8,14 +8,14 @@
             for (i = 0; input[i][1] != '1'; i++)
                 continue;
             i--;
-            var crates = new Stack<char>[(input[i].Length / 4) + 1];
-            for (int j = 0; j < crates.Length; j++)
-                crates[j] = new Stack<char>();
+            var stacks = new Stack<char>[(input[i].Length / 4) + 1];
+            for (int j = 0; j < stacks.Length; j++)
+                stacks[j] = new Stack<char>();
             for (; i >= 0; i--)
                 for (int j = 1; j < input[i].Length; j += 4)
                     if (input[i][j] != ' ')
-                        crates[j / 4].Push(input[i][j]);
-            return crates;
+                        stacks[j / 4].Push(input[i][j]);
+            return stacks;
         }
         public static void MoveCrates(string[] input, Stack<char>[] stacks)
         {
