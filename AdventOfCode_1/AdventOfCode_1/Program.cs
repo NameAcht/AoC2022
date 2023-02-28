a0a6ca34bc4fc9c3a9d770734bde67b43b2ebba7
NameAcht <appeljohannes21@gmail.com>
2023-02-28 22:13:12 +0100

    1

---
+++ AdventOfCode_1/AdventOfCode_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCodeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            int max = 0;
+            int curr = 0;
+            foreach(var line in input)
+                if (int.TryParse(line, out int cal))
+                    curr += cal;
+                else {
+                    max = Math.Max(max, curr);
+                    curr = 0;
+                }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode_1_2/AdventOfCode_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCodeDay1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            var elves = new List<int>();
+            int curr = 0;
+            foreach (var line in input)
+                if (int.TryParse(line, out int cal))
+                    curr += cal;
+                else {
+                    elves.Add(curr);
+                    curr = 0;
+                }
+            elves.Sort();
+            Console.WriteLine(elves[elves.Count - 1] + elves[elves.Count - 2] + elves[elves.Count - 3]);
+            Console.ReadLine();
+        }
+    }
+}
